7ceb9f2b9754bd7ece56c10c98fe11e8d132a8d2
unknown <miguelsoto3211@gmail.com>
2022-06-02 12:45:18 +0900

    New files 20220602

---
+++ HTML/Pratice/20220602/Java Script 勉強/index.js
@@ -0,0 +1,27 @@
+//Writing single header and text
+document.write('<h1>Hello world</h1>');
+document.write('<br>');
+document.write('Single quote text')
+document.write('<br>');
+document.write('<i>what do you think your doing?</i>');
+document.write('<br>');
+document.write('it\s a dog');
+document.write('<br>');
+document.write("it's a dog");
+document.write('<br>');
+
+//Arithmetic Operation
+document.write("1+2=");
+document.write(1+2);
+document.write("<br>")
+document.write("3*5=");
+document.write(3*5);
+document.write("<br>")
+document.write(4/2);
+document.write("<br>")
+document.write(5%2);
+document.write("<br>")
+document.write(5**3);
+document.write("<br>")
+
+//More difficult stuff 
---
+++ HTML/Pratice/20220602/Java Script 勉強/index01.js
@@ -0,0 +1,13 @@
+document.write("<h2>Concatination</h2>");
+document.write("1+2="); //(1 + 2)); // (1 + 2)
+document.write("<br>");
+
+document.write("1+2="+(1+2));
+document.write("<br>");
+
+document.write("1+2="+ 3)
+document.write("<br>");
+
+document.write("<br>");
+document.write("1 + 2 = " + 1 + 2);
+document.write("<br>");
